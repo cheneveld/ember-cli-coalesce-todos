0d66660a8ada87ca6480a095dc9a1bb553315a9f
Craig Heneveld <craig.heneveld@adeptmobi.com>
2014-10-20 10:21:37 -0600

    working stuff

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -10,6 +10,28 @@ export default Ember.Route.extend({
             var self = this;
             var user = this.session.create('user', this.get("controller").getProperties("name"));
 
+            console.log('session about to be stored.');
+
+            var modelsArray = self.session.models.toArray();
+
+            console.log(modelsArray[0]);
+
+           // var serializerFactory = new Coalesce.SerializerFactory(this.container);
+
+            var serialized = [];
+
+            serialized = modelsArray.map(function(model){
+                var serializer =  self.container.lookup('serializer:model');
+
+                return serializer.serialize(model);
+            });
+
+            localforage.setItem('session_models', serialized).then(function(value) {
+                alert(value + ' was set!');
+            }, function(error) {
+                console.error(error);
+            });
+
             self.session.flush().then(function(models) {
                 var savedUser = models[0];
                 self.get("controller.model").pushObject(savedUser);
