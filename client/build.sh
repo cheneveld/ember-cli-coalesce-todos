024aed8ddb3b54adf2c9147ba505113ef30b04ed
Gordon L. Hempton <ghempton@gmail.com>
2014-10-21 19:11:58 -0700

    fix build script to include local coalesce with local coalesce-ember

--- client/build.sh
+++ client/build.sh
@@ -3,7 +3,10 @@
 cd ../../coalesce;
 npm run-script build;
 cd ../coalesce-ember;
+# Hack to use the latest version of Coalesce
+cp ../coalesce/dist/* bower_components/coalesce/
 npm run-script build;
 cd ../ember-cli-coalesce-todos/client;
+mkdir -p vendor/coalesce-ember
 cp -R ../../coalesce-ember/dist/* vendor/coalesce-ember;
 ember build;
